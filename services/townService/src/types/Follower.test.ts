60337514ff8660dcb6adedc0b5255fcaaae81602
jamesdgriffin <griffin.jame@northeastern.edu>
2022-04-15 22:40:18 -0400

    tests for Follower

--- services/townService/src/types/Follower.test.ts
+++ services/townService/src/types/Follower.test.ts
@@ -64,17 +64,41 @@ describe('Follower', () => {
       testingTown.addFollower(player2, player2.id);
       expect(player2.follower?.userName).toBe('Pet');
     });
-  });
+    it('addFollower should not add more than seven followers to a player', () => {
+      let player1 = new Player('first test player');
+      testingTown.addPlayer(player1);
 
-  describe('removing a follower', () => {
-    it('one follower should be removed properly', () => {
-      const player = new Player('test player');
-      const pet = new Player('test pet');
-      player.follower = pet;
-      expect(player.follower?.userName).toEqual(pet.userName);
+      testingTown.addFollower(player1, player1.id);
+      testingTown.addFollower(player1, player1.id);
+      testingTown.addFollower(player1, player1.id);
+      testingTown.addFollower(player1, player1.id);
+      testingTown.addFollower(player1, player1.id);
+      testingTown.addFollower(player1, player1.id);
+      testingTown.addFollower(player1, player1.id);
+      testingTown.addFollower(player1, player1.id);
 
-      player.follower = undefined;
-      expect(player.follower).toBe(undefined);
+      let currentDepth = 0;
+      while (player1.follower !== undefined) {
+        player1 = player1.follower;
+        currentDepth += 1;
+      }
+
+      expect(currentDepth).toEqual(7);
+    });
+    it('should only add a follower if the player is in a pet area', () => {
+      let player1 = new Player('first test player');
+      testingTown.addPlayer(player1);
+    });
+    it('should allow multiple players on the map to have pets', () => {
+      let player1 = new Player('first test player');
+      let player2 = new Player('second test player');
+      testingTown.addPlayer(player1);
+      testingTown.addPlayer(player2);
+      testingTown.addFollower(player1, player1.id);
+      testingTown.addFollower(player1, player1.id);
+      testingTown.addFollower(player2, player2.id);
+      expect(player1.follower?.userName).toBe('Pet');
+      expect(player2.follower?.userName).toBe('Pet');
     });
   });
 });
